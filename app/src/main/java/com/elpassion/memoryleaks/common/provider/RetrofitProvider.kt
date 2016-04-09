8d277cccc4ac2ff7fff8fc1ea794da32758f1ef3
Karol Kowalski <ordon1313@gmail.com>
2016-04-09 21:58:16 +0200

    Add logging interceptor

--- app/src/main/java/com/elpassion/memoryleaks/common/provider/RetrofitProvider.kt
+++ app/src/main/java/com/elpassion/memoryleaks/common/provider/RetrofitProvider.kt
@@ -1,5 +1,7 @@
 package com.elpassion.memoryleaks.common.provider
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,10 +11,14 @@ object RetrofitProvider {
     private val instance by lazy {
         Retrofit.Builder()
                 .baseUrl("https://google.com")
+                .client(createOkHttpClient())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
+
+    private fun createOkHttpClient() =
+            OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }).build()
 
     fun getRetrofit(): Retrofit {
         return instance
