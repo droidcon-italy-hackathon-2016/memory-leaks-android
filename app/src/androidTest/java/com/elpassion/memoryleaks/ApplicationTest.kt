a3eb8843861b143f06756a00d67af51614085f4f
Karol Kowalski <ordon1313@gmail.com>
2016-04-09 12:04:28 +0200

    all in kotlin

---
+++ app/src/androidTest/java/com/elpassion/memoryleaks/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.elpassion.memoryleaks
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
