a279e3c5acdfd2508264556a293c9f920cdbc773
Maciej Górski <maciek.gorski@gmail.com>
2016-04-09 15:12:49 +0200

    Add confirmation screen.

---
+++ app/src/main/java/com/elpassion/memoryleaks/confirmation/view/impl/ConfirmationActivity.kt
@@ -0,0 +1,13 @@
+package com.elpassion.memoryleaks.confirmation.view.impl
+
+import android.os.Bundle
+import com.elpassion.memoryleaks.R
+import com.elpassion.memoryleaks.common.android.BaseActivity
+
+class ConfirmationActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.confirmation_activity)
+    }
+}
