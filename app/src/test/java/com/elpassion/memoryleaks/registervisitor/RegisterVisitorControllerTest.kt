40fd192b8721382703825f666ebae59992886178
Maciej Górski <maciek.gorski@gmail.com>
2016-04-09 14:42:15 +0200

    [refactoring] Move controller and view to main.

--- app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
+++ app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
@@ -11,7 +11,7 @@ import rx.Observable.just
 class RegisterVisitorControllerTest {
 
     private val apiCall: () -> Observable<Unit> = mock()
-    private val view: RegistrationVisitorView = mock()
+    private val view: RegisterVisitorView = mock()
     private val controller = RegisterVisitorController(apiCall, view)
 
     @Test
@@ -33,29 +33,5 @@ class RegisterVisitorControllerTest {
         whenever(apiCall.invoke()).thenReturn(error(RuntimeException()))
         controller.onRegisterClick()
         verify(view).showError()
-    }
-}
-
-interface  RegistrationVisitorView {
-
-    fun showConfirmationScreen()
-
-    fun showError()
-}
-
-class RegisterVisitorController(
-        private val apiCall: () -> Observable<Unit>,
-        private val view: RegistrationVisitorView) {
-
-    fun onRegisterClick() {
-        apiCall.invoke().subscribe(onSuccess, onError)
-    }
-
-    val onSuccess = { unit: Unit ->
-        view.showConfirmationScreen()
-    }
-
-    val onError = { ex: Throwable ->
-        view.showError()
     }
 }
