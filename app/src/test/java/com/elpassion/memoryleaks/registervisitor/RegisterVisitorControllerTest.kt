8ad4e23f4e95d12d0d5f323bbdd58eeb53f47078
Maciej Górski <maciek.gorski@gmail.com>
2016-04-09 14:06:44 +0200

    Should show confirmation screen.

--- app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
+++ app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
@@ -2,22 +2,44 @@ package com.elpassion.memoryleaks.registervisitor
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
+import rx.Observable
+import rx.Observable.just
 
 class RegisterVisitorControllerTest {
 
-    private val apiCall: () -> Unit = mock()
+    private val apiCall: () -> Observable<Unit> = mock()
+    private val view: RegistrationVisitorView = mock()
+    private val controller = RegisterVisitorController(apiCall, view)
 
     @Test
     fun shouldInvokeApiCall() {
-        RegisterVisitorController(apiCall).onRegisterClick()
+        whenever(apiCall.invoke()).thenReturn(just(Unit))
+        controller.onRegisterClick()
         verify(apiCall).invoke()
+    }
+
+    @Test
+    fun shouldShowConfirmationScreen() {
+        whenever(apiCall.invoke()).thenReturn(just(Unit))
+        controller.onRegisterClick()
+        verify(view).showConfirmationScreen()
     }
 }
 
-class RegisterVisitorController(private val apiCall: () -> Unit) {
+interface  RegistrationVisitorView {
+
+    fun showConfirmationScreen()
+}
+
+class RegisterVisitorController(
+        private val apiCall: () -> Observable<Unit>,
+        private val view: RegistrationVisitorView) {
 
     fun onRegisterClick() {
-        apiCall.invoke()
+        apiCall.invoke().subscribe {
+            view.showConfirmationScreen()
+        }
     }
 }
