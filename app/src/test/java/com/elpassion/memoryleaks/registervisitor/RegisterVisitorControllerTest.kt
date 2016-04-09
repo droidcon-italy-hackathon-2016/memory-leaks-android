a4e6a35cb1631a8e63312ba5a0ffc0da10e821cc
Maciej Górski <maciek.gorski@gmail.com>
2016-04-09 13:40:51 +0200

    Should invoke api call.

--- app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
+++ app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
@@ -1,15 +1,23 @@
 package com.elpassion.memoryleaks.registervisitor
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class RegisterVisitorControllerTest {
 
+    private val apiCall: () -> Unit = mock()
+
     @Test
-    fun shouldCreateController() {
-        RegisterVisitorController()
+    fun shouldInvokeApiCall() {
+        RegisterVisitorController(apiCall).onRegisterClick()
+        verify(apiCall).invoke()
     }
 }
 
-class RegisterVisitorController {
+class RegisterVisitorController(private val apiCall: () -> Unit) {
 
+    fun onRegisterClick() {
+        apiCall.invoke()
+    }
 }
