564267ee58f8c1eadc610647295cc3a5e7c03e0e
Maciej Górski <maciek.gorski@gmail.com>
2016-04-09 13:37:44 +0200

    Should create controller.

---
+++ app/src/test/java/com/elpassion/memoryleaks/registervisitor/RegisterVisitorControllerTest.kt
@@ -0,0 +1,15 @@
+package com.elpassion.memoryleaks.registervisitor
+
+import org.junit.Test
+
+class RegisterVisitorControllerTest {
+
+    @Test
+    fun shouldCreateController() {
+        RegisterVisitorController()
+    }
+}
+
+class RegisterVisitorController {
+
+}
