8b5aaecfcbef39cf7dd5908fd55ffb45b682dfd4
Maciej Górski <maciek.gorski@gmail.com>
2016-04-10 13:25:17 +0200

    Knock - knock.

--- app/src/main/java/com/elpassion/memoryleaks/usecase/ping/api/PingApiCallProvider.kt
+++ app/src/main/java/com/elpassion/memoryleaks/usecase/ping/api/PingApiCallProvider.kt
@@ -2,6 +2,7 @@ package com.elpassion.memoryleaks.usecase.ping.api
 
 import com.elpassion.memoryleaks.common.provider.RetrofitProvider.getRetrofit
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 import rx.Observable
 
@@ -13,7 +14,7 @@ object PingApiCallProvider {
 
     private interface Api {
 
-        @GET("ping")
+        @POST("knock_knock")
         fun call(@Query("elder_id") elderId: String): Observable<Unit>
     }
 }
