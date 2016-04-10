8487d49fe6e1895d0973b949cdd7b96d58ae9a48
Karol Kowalski <ordon1313@gmail.com>
2016-04-10 13:24:34 +0200

    id

--- app/src/main/java/com/elpassion/memoryleaks/model/Elder.kt
+++ app/src/main/java/com/elpassion/memoryleaks/model/Elder.kt
@@ -2,4 +2,4 @@ package com.elpassion.memoryleaks.model
 
 import com.google.gson.annotations.SerializedName
 
-class Elder(@SerializedName("photo_url") val photoUrl: String)
+class Elder(val id: String, @SerializedName("photo_url") val photoUrl: String)
